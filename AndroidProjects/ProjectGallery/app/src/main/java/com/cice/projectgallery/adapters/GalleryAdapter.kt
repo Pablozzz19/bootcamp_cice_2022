075dd56b1c65001e25fd0ea10e8328398b6f090e
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2022-01-25 13:36:26 +0100

    [WIP] - Fill RecyclerView

---
+++ AndroidProjects/ProjectGallery/app/src/main/java/com/cice/projectgallery/adapters/GalleryAdapter.kt
@@ -0,0 +1,48 @@
+package com.cice.projectgallery.adapters
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.cice.projectgallery.R
+
+class GalleryAdapter(private val context : Context, private val dataSet : ArrayList<Uri>) :
+    RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val ivPhoto : ImageView
+
+        init {
+            ivPhoto = view.findViewById(R.id.ivPhoto)
+        }
+
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.image_row_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        //holder.ivPhoto.setImageResource(dataSet[position])
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+}
