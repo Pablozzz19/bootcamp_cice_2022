dcd67e32b8162a0370783402d058f22ec3fed0df
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2022-01-25 21:44:25 +0100

    [WIP]

---
+++ MyAppIntroductionTableView/MyAppIntroductionTableView/Sources/Modules/ListaPerfilPostView/Cell/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  MyAppIntroductionTableView
+//
+//  Created by cice on 25/01/2022.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- MyAppIntroductionTableView/MyAppIntroductionTableView/Sources/Modules/ListaPerfilPostView/ListaPerfilPostViewController.swift
+++ MyAppIntroductionTableView/MyAppIntroductionTableView/Sources/Modules/ListaPerfilPostView/ListaPerfilPostViewController.swift
@@ -9,21 +9,70 @@ import UIKit
 
 class ListaPerfilPostViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var miPerfilPostTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.configurationTableView()
         // Do any additional setup after loading the view.
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configurationTableView() {
+        self.miPerfilPostTableView.delegate = self
+        self.miPerfilPostTableView.dataSource = self
+        self.miPerfilPostTableView.register(UINib(nibName: PerfilCell.defaultReuseIdentifier, bundle: nil), forCellReuseIdentifier: PerfilCell.defaultReuseIdentifier)
     }
-    */
 
+}
+
+extension ListaPerfilPostViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        default:
+            return 10
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let cellPerfil = self.miPerfilPostTableView.dequeueReusableCell(withIdentifier: PerfilCell.defaultReuseIdentifier, for: indexPath) as! PerfilCell
+            cellPerfil.setupCellPerfil(data: UserDataModel(nombrePerfil: "Pablo Tesheiner Moreno",
+                                                           descripcionPerfil: "Lrem ipsum donde las leyes haces lo que quieran Lrem ipsum donde las leyes haces lo que quieran Lrem ipsum donde las leyes haces lo que quieran",
+                                                           puestoActualPerfil: "Estudiante CICE",
+                                                           usuarioLinkedInPerfil: "@PabloTesheiner",
+                                                           imagePerfil: "thor"))
+            return cellPerfil
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+}
+
+extension ListaPerfilPostViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 266
+        default:
+            return 44
+        }
+    }
+    
 }
