27643e3de5243996675acafbbffe1b72725efef1
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2022-01-26 10:41:33 +0100

    [WIP] - Finish Details

--- AndroidProjects/ProjectGallery/app/src/main/java/com/cice/projectgallery/MainActivity.kt
+++ AndroidProjects/ProjectGallery/app/src/main/java/com/cice/projectgallery/MainActivity.kt
@@ -47,6 +47,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        readGallery()
         binding.btnCamera.setOnClickListener {
             checkCameraPermission()
         }
