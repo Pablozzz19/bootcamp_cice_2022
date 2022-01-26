2c81b7ff16b985881aad3350e21d8039a233a47b
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2022-01-26 21:58:22 +0100

    [WIP] - MyUserDefault create RegisterViewController

--- MyUserDefault/MyUserDefault/Sources/RegisterView/RegisterViewController.swift
+++ MyUserDefault/MyUserDefault/Sources/RegisterView/RegisterViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    // MARK: - Variables globales
+    var existeFoto = false
+    var usuarioLogado = false
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var nombreTF: UITextField!
+    @IBOutlet weak var apellidoTF: UITextField!
+    @IBOutlet weak var direccionTF: UITextField!
+    @IBOutlet weak var telefonoTF: UITextField!
+    @IBOutlet weak var imagenPerfilTF: UIImageView!
+    @IBOutlet weak var fechaActualizacionLBL: UILabel!
+    
+    // MARK: - IBActions
+    @IBAction func guardarDatosACTION(_ sender: Any) {
+    }
+    
+    @IBAction func recuperarDatosACTION(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
