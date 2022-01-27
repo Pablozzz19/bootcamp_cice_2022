1216d783d09b4adbf317d02d298977c01ee68c61
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2022-01-27 09:47:07 +0100

    [WIP] - Create MVVM User

---
+++ AndroidProjects/ProjectShop/app/src/main/java/com/cice/projectshop/user/model/UserProvider.kt
@@ -0,0 +1,22 @@
+package com.cice.projectshop.user.model
+
+class UserProvider {
+
+    companion object {
+
+        fun writeNewUserModel() {
+
+            val userModel = UserModel(id = "",
+                email = "",
+                fullName = "",
+                birthdate = "",
+                description = "",
+                gender = "")
+
+
+
+        }
+
+    }
+
+}
