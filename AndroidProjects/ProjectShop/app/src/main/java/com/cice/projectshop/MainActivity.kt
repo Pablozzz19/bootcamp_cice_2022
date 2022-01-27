fb52c458e64a73242949fd4e6200d34c7990b226
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2022-01-27 11:40:06 +0100

    [WIP] - Finish fragment_register.xml

--- AndroidProjects/ProjectShop/app/src/main/java/com/cice/projectshop/MainActivity.kt
+++ AndroidProjects/ProjectShop/app/src/main/java/com/cice/projectshop/MainActivity.kt
@@ -2,7 +2,11 @@ package com.cice.projectshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
 import com.cice.projectshop.databinding.ActivityMainBinding
+import com.cice.projectshop.fragments.register.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +17,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setFragment(RegisterFragment())
 
+    }
 
+    fun setFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.fcvContainerRegister, fragment, fragment.javaClass.toString())
+            setReorderingAllowed(true)
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            addToBackStack(null)
+        }
     }
 }
