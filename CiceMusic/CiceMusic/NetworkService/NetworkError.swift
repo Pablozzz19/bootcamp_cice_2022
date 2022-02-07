69496478252677bab002b9dd44a0bcecf248788e
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2022-02-07 19:15:03 +0100

    [WIP] - Added SplashProvider

--- CiceMusic/CiceMusic/NetworkService/NetworkError.swift
+++ CiceMusic/CiceMusic/NetworkService/NetworkError.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Andres Felipe Ocampo Eljaiek on 21/5/21.
 //
-
 import Foundation
 
 public struct NetworkError: Error, LocalizedError {
---
+++ CiceMusic/CiceMusic/Sources/SplashView/SplashProvider.swift
@@ -0,0 +1,35 @@
+//
+//  SplashProvider.swift
+//  CiceMusic
+//
+//  Created by cice on 07/02/2022.
+//
+
+import Foundation
+
+protocol SplashProviderInputProtocol {
+    func fetchData()
+}
+
+final class SplashProvider: SplashProviderInputProtocol {
+    
+    let networkservice: NetworkServiceProtocol = NetworkService()
+    
+    func fetchData() {
+        self.networkservice.requestGeneric(requestPayload: SplashRequestDTO.requestData(numeroItems: "10"),
+                                           entityClass: <#T##Decodable.Protocol#>,
+                                           success: <#T##(Decodable?) -> Void#>,
+                                           failure: <#T##(NetworkError) -> Void#>)
+    }
+}
+
+
+struct SplashRequestDTO {
+    
+    static func requestData(numeroItems: String) -> RequestDTO {
+        let argument: [CVarArg] = [numeroItems]
+        let urlComplete = String(format: URLEnpoint.music, arguments: argument)
+        let request = RequestDTO(params: nil, method: .get, endpoint: urlComplete)
+        return request
+    }
+}
